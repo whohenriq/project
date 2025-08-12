ff590063e257860bc059ba87e10a51b2f57fa2d2
Henrique Eduardo <whohenrique.e@gmail.com>
2025-08-12 15:44:45 -0300

    feat: add social login authentication with supabase API

---
+++ src/hooks/useAuth.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import type React from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  useCallback,
+} from "react";
+import { supabase } from "@/lib/supabaseClient";
+import { User } from "@/types/uset";
+
+interface AuthContextType {
+  user: User | null;
+  isLoading: boolean;
+  signOut: () => Promise<void>;
+  signInWithGoogle: () => Promise<void>;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function mapSupabaseUser(supabaseUser: any): User {
+  return {
+    id: supabaseUser.id,
+    email: supabaseUser.email || "",
+    username:
+      supabaseUser.user_metadata?.user_name ||
+      supabaseUser.user_metadata?.full_name ||
+      supabaseUser.email?.split("@")[0] ||
+      "unknown",
+    avatar: supabaseUser.user_metadata?.avatar_url || undefined,
+  };
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getUser = async () => {
+      setIsLoading(true);
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      if (user) {
+        setUser(mapSupabaseUser(user));
+      } else {
+        setUser(null);
+      }
+      setIsLoading(false);
+    };
+    getUser();
+
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      (_event, session) => {
+        if (session?.user) {
+          setUser(mapSupabaseUser(session.user));
+        } else {
+          setUser(null);
+        }
+      }
+    );
+    return () => {
+      listener?.subscription.unsubscribe();
+    };
+  }, []);
+  const signOut = useCallback(async () => {
+    setIsLoading(true);
+    await supabase.auth.signOut();
+    setUser(null);
+    setIsLoading(false);
+  }, []);
+
+  const signInWithGoogle = useCallback(async () => {
+    setIsLoading(true);
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `${window.location.origin}`,
+      },
+    });
+    if (error) throw error;
+    setIsLoading(false);
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ user, signOut, isLoading, signInWithGoogle }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+
+  if (context === undefined) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+
+  return context;
+}
